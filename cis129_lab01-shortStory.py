3983502cda2c10e49ba8b3c363d82d0bd677f1ee
gmarin1995 <160804128+gmarin1995@users.noreply.github.com>
2024-02-21 19:22:01 -0700

    Update cis129_lab01-shortStory.py
    Removed the initial print command and instead added a short story. Also added a brief description of myself.

--- cis129_lab01-shortStory.py
+++ cis129_lab01-shortStory.py
@@ -1,2 +1,6 @@
-#Just a simple python "script"
-print('Hello, World!')
+#Author: Gerardo Marin
+#First Python Lab for CIS129
+#This prints a simple short story
+print('Once upon a time, a bright-eyed college student called Emily started her adventure into coding in a busy dorm. Equipped with a strong will and inquisitiveness, she established her initial GitHub account and embarked on a mission to master the realm of programming.'
+print('She typed her first "Hello, World!" script with apprehension, an exciting but modest first step that signaled the start of her coding journey. 
+print('She had no idea that this little script would spark a love for coding that would have a profound impact on her future.')
